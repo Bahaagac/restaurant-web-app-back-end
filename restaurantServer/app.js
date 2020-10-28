06c57b188b345524f408ce69a4431f4da1c075ac
Bahaagac <48641679+Bahaagac@users.noreply.github.com>
2020-10-28 22:55:02 +0300

    Mongoose DishId/Comments

--- restaurantServer/app.js
+++ restaurantServer/app.js
@@ -10,6 +10,16 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+    console.log("Connected correctly to server");
+}, (err) => { console.log(err); });
 
 
 var app = express();
@@ -34,7 +44,6 @@ app.use('/leaders',leaderRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
